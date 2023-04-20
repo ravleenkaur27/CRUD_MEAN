d7d87a7e7d775755339b0fe59fa172af7897e3b9
Ravleen <ravleenkaur2703@gmail.com>
2023-04-20 22:25:42 +0530

    Second Commit

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const app = express()
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
 
+const Article = require('/home/ravleen/Desktop/mean_project/models/article')
+const articleRouter = require('/home/ravleen/Desktop/mean_project/routes/articles')
+const app = express()
+var db = require('./config/db');
+console.log("connecting--",db);
+mongoose.connect(db.url); //Mongoose connection created
 app.set('view engine', 'ejs')
-mongoose.connect('mongodb://localhost/meanblog')
-app.use('/articles',articleRouter)
+
+
 app.use(express.urlencoded({ extended: false }))
+app.use('/articles',articleRouter)
+
 app.get('/', (req,res) => {
 const articles = [{
 title: "Test",
@@ -22,5 +27,5 @@ description: "LOL 2"
 ]
 res.render('articles/index', {articles: articles})
 })
-
+app.use('/articles',articleRouter)
 app.listen(3000)
